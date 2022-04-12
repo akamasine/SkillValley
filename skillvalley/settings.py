06d3fe81f7483370f238dff95ee9700c6037fb74
Nischal Gautam <60080619+akamasine@users.noreply.github.com>
2022-04-12 21:34:12 +0545

    Update settings.py

--- skillvalley/settings.py
+++ skillvalley/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
@@ -123,7 +123,7 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 
 STATIC_URL = '/static/'
 
